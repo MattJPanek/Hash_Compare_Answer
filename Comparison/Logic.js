4e3e4fb9c9b25b94501f592313df4963ccb24875
Matthew Panek <matthewpanek@protonmail.com>
2022-11-11 16:31:40 -0600

    Adding Logic Controller

---
+++ Comparison/Logic.js
@@ -0,0 +1,23 @@
+import Formulas from "Comparison";
+
+
+class Logic{
+    
+    compare(source,target){
+        let objArr = [];
+        const formula = new Formulas();
+    if(formula.checkIfShallow(source,target) == false){
+
+        let objArr = deepCompare(source, target);
+
+    }else{
+
+        let objArr = shallowCompare(source, target);
+
+    }
+
+        return objArr;
+    }
+
+
+}
